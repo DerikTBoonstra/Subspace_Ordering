95d0b7e41af66de8d5323e2813a895717479872e
D3r1k <derik.todd@gmail.com>
2025-11-24 17:24:00 -0600

    Update boston_housing.R

--- real_data_applications/boston_housing.R
+++ real_data_applications/boston_housing.R
@@ -3,6 +3,8 @@ library("tidyverse");theme_set(theme_minimal());library("patchwork")
 
 # Load data and Train/Test Split --------------------------------------------------------
 data("BostonHousing", package = "mlbench")
+BostonHousing <- BostonHousing |> 
+  filter(crim < 3.2)
 set.seed(1)
 split <- rsample::initial_split(BostonHousing, prop = 0.80)
 train <- rsample::training(split)
@@ -10,14 +12,14 @@ test <- rsample::testing(split)
 
 
 # SDR Fitting -------------------------------------------------------------
-out <- sdr::sdr(train[,-14], train$medv, method = "save")
+out <- sdr::sdr(train[,-14], train$medv, method = "pca")
 df_train <- tibble("y" = out$y, 
                    "x" = out$ProjectedData[,1])
 
 f_out <- sdr::dim_order(object = out, method = "F")
 
 # Analysis ----------------------------------------------------------------
-## 4th SAVE subspace is the most informative
+## 11th PCA subspace is the most informative
 f_out$dims 
 f_out$dim_criteria
 
@@ -28,21 +30,20 @@ test_x <- apply(as.matrix(test[,-14]), 2, as.numeric)
 df_test <- tibble("y" = test$medv, "x" = as.numeric(test_x %*% as.matrix(out$ProjectionMatrix[,1])))
 df_test_f <- tibble("y" = test$medv, "x" = as.numeric(test_x %*% as.matrix(f_out$ProjectionMatrix[,1])))
 
-## variability comparisoon
+## variability comparison
 df_test$x |> var()
 df_test_f$x |> var()
 
 ## Model fitting
-mod <- lm(y~., data = df_train)
-mod_f <- lm(y~ poly(x, 2), data = df_train_f)
+mod <- lm(y~ x, data = df_train)
+mod_f <- lm(y~ x, data = df_train_f)
 
-## Insignificant predictor
+## PCA
 summary(mod)
 Metrics::mse(df_test$y, predict(mod, newdata = df_test))
-## Worse than just the mean
-Metrics::mse(df_test$y, mean(df_train$y))
 
-# Significant and Improves prediction
+
+# PCA_F
 summary(mod_f)
 Metrics::mse(df_test_f$y, predict(mod_f, newdata = df_test_f))
 
@@ -51,13 +52,16 @@ Metrics::mse(df_test_f$y, predict(mod_f, newdata = df_test_f))
 p1 <-  df_test |> 
   ggplot(aes(x, y))+
   geom_point()+
-  labs(x = "SAVE 1st Subspace", y = "Median Housing Price")
+  geom_line(aes(y = predict(mod, newdata = df_test)), color = "blue")+
+  labs(x = "PCA 1st Subspace", y = "Median Housing Price")
   
 p2 <- df_test_f |> 
   ggplot(aes(x, y))+
   geom_point()+
-  labs(x = expression("SAVE"["F"]*" 1st Subspace"))+
+  geom_line(aes(y = predict(mod_f, newdata = df_test_f)), color = "red", linetype = "dashed")+
+  labs(x = expression("PCA"["F"]*" 1st Subspace"))+
   theme(axis.text.y = element_blank(), 
         axis.title.y = element_blank(), 
         axis.ticks.y = element_blank())
 p1 + p2
+
